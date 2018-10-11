5c5488951ed55e59d87a61e6f7b1e28c662496c5
stonepc-mi <great_cy_ang@163.com>
2018-10-11 17:30:36 +0800

    update transaction.go

--- chaincode/transaction.go
+++ chaincode/transaction.go
@@ -7,39 +7,22 @@ import (
     "crypto/rsa"
     "crypto/rand"
     "crypto/sha256"
+    "io/ioutil"
     "fmt"
 
     "github.com/hyperledger/fabric/core/chaincode/shim"
     pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-var privatekey = []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQCz+PeAz/FHi2FpdxWkRz12/WbOItZtOl479dMs4O5BAOwERBqJ
-mEjnL7STL30X630mZal6hMjM5N8K6LTRA2YCqaIdLvrXQSAHbmQ7Dt79ETnrNztY
-zcNz926+P402toOpWGcLP3AtJ6Oc8cRSFFTiF33HDaBIQxRnnVO7FdXFzwIDAQAB
-AoGAFFqwA02BSEonNjpVpEK6XN7D5cRi4++aAMYIoCbAS3HDP6hEKBOlCyCGF69j
-QnVLrjAJPuYNn76yyxUOfiUYQDw96Ij6oGBhO2KvKswXBU/V2XipUUF4C21/EGdk
-FtVTHCEPJ6ciaFUUcVGuMRWt2H3z9Jk0xXpCxgGMeTRgTaECQQDgQM00marIVE09
-EIHXnXBJJAueNXCOnft4JCrSE5jv/UX8yNNjHeKSJkbRfw5oxSZ8OClSyy9qdjJA
-eOSMbWDFAkEAzXNiQLqmonZ+g010lCvPsBNXbdKV8tcbMJht5ywI7zafcv2og/g7
-rlSi3dXRAasmi4HDZ+R6R0VdsVHU/NpNgwJBAK3sFDrTY0zzdOQDRXCAPnG7bvdI
-4v75L+tBwaQkZtzaRcmDx857gxlubkZUkExZezukIwh/ZUrgWKEAIeF3gzECQErT
-lFxY3qnbGFbAFg6FKl5JnRUdlolceMWpLpX8fsCJF2etJPvzo+DpaGv9HONLr30t
-5LppB3P/upEDadwxsfsCQQDCBOHd3ydJXWXftNyAqorDdQ0AVdU6f9pcfacKkD5K
-cUoVOemDN5dcyfF4sHhkt484nFTfvKaACeEcgrJGIsuf
------END RSA PRIVATE KEY-----`)
-var publickey = []byte(`-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCz+PeAz/FHi2FpdxWkRz12/WbO
-ItZtOl479dMs4O5BAOwERBqJmEjnL7STL30X630mZal6hMjM5N8K6LTRA2YCqaId
-LvrXQSAHbmQ7Dt79ETnrNztYzcNz926+P402toOpWGcLP3AtJ6Oc8cRSFFTiF33H
-DaBIQxRnnVO7FdXFzwIDAQAB
------END PUBLIC KEY-----`)
-
-
 type TxCC struct {
 }
 
-func makeAddress(publicKey []byte) string {
+func readFileAsByte(filePath string) []byte {
+    b, _ := ioutil.ReadFile(filePath)
+    return b
+}
+
+func makeAddress(publickey []byte) string {
     h := sha256.New()
     h.Write(publickey)
     addr := base64.StdEncoding.EncodeToString(h.Sum(nil))
@@ -54,36 +37,74 @@ func Encryption(plaintext []byte, publickey []byte) []byte {
     return cipherdata
 }
 
-func Decryption(ciphertext []byte, privatekey []byte) string {
-   // 获取pem格式的私钥
-   pemkey,_ := pem.Decode(privatekey)
-   // 解析PKCS1格式的私钥
-   newprivatekey,_ := x509.ParsePKCS1PrivateKey(pemkey.Bytes)
-   // 解密
-   plaintext, _ := rsa.DecryptPKCS1v15(rand.Reader,newprivatekey,ciphertext)
-   return string(plaintext)
+func Decryption(cipherdata []byte, privatekey []byte) string {
+    // 获取pem格式的私钥
+    pemkey,_ := pem.Decode(privatekey)
+    // 解析PKCS1格式的私钥
+    newprivatekey,_ := x509.ParsePKCS1PrivateKey(pemkey.Bytes)
+    // 解密
+    plaintext, _ := rsa.DecryptPKCS1v15(rand.Reader,newprivatekey,cipherdata)
+    return string(plaintext)
 }
 
+// 初始化
+// args: 0-{pubkey_file_address},1-{plaintext}
 func (t *TxCC) Init(stub shim.ChaincodeStubInterface) pb.Response {
+    _, args := stub.GetFunctionAndParameters()
     // TODO:初始化时给账户A赋值
-    var plaintext = []byte(`A账户的文件`)
+    publickey := readFileAsByte(args[0])
+    plaintext := []byte(args[1])
     addressA := makeAddress(publickey)
     cipherdata := Encryption(plaintext, publickey)
-    stub.PutState(addressA, cipherdata)
+    error := stub.PutState(addressA, cipherdata)
+    if error != nil {
+        shim.Error("PutState failed!")
+    }
     return shim.Success(nil)
 }
 
+// 交易
+// args: 0-{pubkey_file_addressA},1-{pubkey_file_addressB},2-{prikey_file_addressA}
 func (t *TxCC) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
     // TODO:解密账户A的数据，同时将数据用B的公钥加密，存入B的地址
+    publickeyA := readFileAsByte(args[0])
+    publickeyB := readFileAsByte(args[1])
+    privatekeyA := readFileAsByte(args[2])
+    addressA := makeAddress(publickeyA)
+    addressB := makeAddress(publickeyB)
+    cipherdataA, error := stub.GetState(addressA)
+    if error != nil {
+        shim.Error("GetState failed!")
+    }
+    plaintext := Decryption(cipherdataA, privatekeyA)
+    cipherdataB := Encryption([]byte(plaintext), publickeyB)
+
+    error = stub.PutState(addressB, cipherdataB)
+    if error != nil {
+        shim.Error("PutState failed!")
+    }
 
     return shim.Success(nil)
 }
 
+// 查询
+// args: 0-{pubkey_file_address},1-{prikey_file_address}
 func (t *TxCC) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
     // TODO:根据公钥找到地址，查询记录，同时使用私钥解密
-    return shim.Success(nil)
+    publickey := readFileAsByte(args[0])
+    privatekey := readFileAsByte(args[1])
+    address := makeAddress(publickey)
+    cipherdata, error := stub.GetState(address)
+    if error != nil {
+        return shim.Error("qurey failed!")
+    }
+
+    plaintext := Decryption(cipherdata, privatekey)
+    fmt.Println(plaintext)
+    return shim.Success([]byte(plaintext))
 }
 
+// Invoke
 func (t *TxCC) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
     function, args := stub.GetFunctionAndParameters()
     if function == "invoke" {
