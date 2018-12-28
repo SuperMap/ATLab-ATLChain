184769505f1a0c435d85c125469703a495bf6b6c
stone <great_cy_ang@163.com>
2018-12-28 14:03:01 +0800

    modify orderer ip in run.sh

--- config/run.sh
+++ config/run.sh
@@ -31,20 +31,20 @@ then
     echo "### export CORE_PEER_MSPCONFIGPATH=$PWD/crypto-config/peerOrganizations/orga.atlchain.com/users/Admin@orga.atlchain.com/msp ###"
     export CORE_PEER_MSPCONFIGPATH=$PWD/crypto-config/peerOrganizations/orga.atlchain.com/users/Admin@orga.atlchain.com/msp
 
-    echo "### peer channel create -o 127.0.0.1:7050 -c atlchannel -f atlchannel.tx ###"
-    peer channel create -o 127.0.0.1:7050 -c atlchannel -f atlchannel.tx
+    echo "### peer channel create -o 148.70.109.243:7050 -c atlchannel -f atlchannel.tx ###"
+    peer channel create -o 148.70.109.243:7050 -c atlchannel -f atlchannel.tx
 
     echo "### peer channel join -b atlchannel.block ###"
     peer channel join -b atlchannel.block
     
-    echo "###  peer channel update -o 127.0.0.1:7050 -c atlchannel -f OrgAanchors.tx ###"
-    peer channel update -o 127.0.0.1:7050 -c atlchannel -f OrgAanchors.tx
+    echo "###  peer channel update -o 148.70.109.243:7050 -c atlchannel -f OrgAanchors.tx ###"
+    peer channel update -o 148.70.109.243:7050 -c atlchannel -f OrgAanchors.tx
 
     echo "### peer chaincode install -n cc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/example02/cmd/ ###"
     peer chaincode install -n cc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/example02/cmd/ 
     
-    echo "### peer chaincode instantiate -o 127.0.0.1:7050 -C atlchannel -n cc -v 1.0 -c '{"Args":["init","a","100","b","200"]}' ###"
-    peer chaincode instantiate -o 127.0.0.1:7050 -C atlchannel -n cc -v 1.0 -c '{"Args":["init","a","100","b","200"]}'
+    echo "### peer chaincode instantiate -o 148.70.109.243:7050 -C atlchannel -n cc -v 1.0 -c '{"Args":["init","a","100","b","200"]}' ###"
+    peer chaincode instantiate -o 148.70.109.243:7050 -C atlchannel -n cc -v 1.0 -c '{"Args":["init","a","100","b","200"]}'
      
     echo "### Finish stage 2 ###"
 elif [ $1 = query ]
@@ -54,5 +54,5 @@ then
 elif [ $1 = invoke ]
 then
     echo "### invoke ###"
-    peer chaincode invoke -o 127.0.0.1:7050 -C atlchannel -n cc -c '{"Args":["invoke","a","b","1"]}'
+    peer chaincode invoke -o 148.70.109.243:7050 -C atlchannel -n cc -c '{"Args":["invoke","a","b","1"]}'
 fi
