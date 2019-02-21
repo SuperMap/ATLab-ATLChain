6eb8451596dc9ce3178a65269bf1e22e89a918c6
stone-ch <great_cy_ang@163.com>
2019-02-21 20:07:58 +0800

    docker compose files

--- docker/scripts/script.sh
+++ docker/scripts/script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#
-# Author: chengyang@supermap.com
-# Date: 2019-02-20
-#
-
 echo
 echo " ____    _____      _      ____    _____ "
 echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
@@ -12,14 +7,14 @@ echo "\___ \    | |     / _ \   | |_) |   | |  "
 echo " ___) |   | |    / ___ \  |  _ <    | |  "
 echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
 echo
-echo "Building the DEMO system"
+echo "Build your first network (BYFN) end-to-end test"
 echo
 CHANNEL_NAME="$1"
 DELAY="$2"
 LANGUAGE="$3"
 TIMEOUT="$4"
 VERBOSE="$5"
-: ${CHANNEL_NAME:="atlchannel"}
+: ${CHANNEL_NAME:="mychannel"}
 : ${DELAY:="3"}
 : ${LANGUAGE:="golang"}
 : ${TIMEOUT:="10"}
@@ -28,7 +23,14 @@ LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=10
 
-CC_SRC_PATH="atlchainCC0_4_1.out"
+CC_SRC_PATH="github.com/chaincode/"
+if [ "$LANGUAGE" = "node" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+fi
+
+if [ "$LANGUAGE" = "java" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/java/"
+fi
 
 echo "Channel name : "$CHANNEL_NAME
 
@@ -39,15 +41,15 @@ createChannel() {
 	setGlobals 0 1
 
 	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
-        set -x
-		peer channel create -o orderer.atlchain.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+                set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
 		res=$?
-        set +x
+                set +x
 	else
-	    set -x
-		peer channel create -o orderer.atlchain.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+				set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
 		res=$?
-	    set +x
+				set +x
 	fi
 	cat log.txt
 	verifyResult $res "Channel creation failed"
@@ -75,39 +77,44 @@ echo "Having all peers join the channel..."
 joinChannel
 
 ## Set the anchor peers for each org in the channel
-echo "Updating anchor peers for orga..."
+echo "Updating anchor peers for org1..."
 updateAnchorPeers 0 1
-echo "Updating anchor peers for orgb..."
+echo "Updating anchor peers for org2..."
 updateAnchorPeers 0 2
 
-## Install chaincode on peer0.orga and peer0.orgb
-echo "Installing chaincode on peer0.orga..."
+## Install chaincode on peer0.org1 and peer0.org2
+echo "Installing chaincode on peer0.org1..."
 installChaincode 0 1
-echo "Install chaincode on peer0.orgb..."
+echo "Install chaincode on peer0.org2..."
 installChaincode 0 2
 
-# Instantiate chaincode on peer0.orgb
-echo "Instantiating chaincode on peer0.orgb..."
+# Instantiate chaincode on peer0.org2
+echo "Instantiating chaincode on peer0.org2..."
 instantiateChaincode 0 2
+sleep $DELAY
 
-# Query chaincode on peer0.orga
-echo "Querying chaincode on peer0.orga..."
+# Invoking chaincode on peer0.org1
+echo "Invoking chaincode on peer0.org1..."
+chaincodeInvoke 0 1
+
+# Query chaincode on peer0.org1
+echo "Querying chaincode on peer0.org1..."
 chaincodeQuery 0 1 100
 
-# Invoke chaincode on peer0.orga and peer0.orgb
-echo "Sending invoke transaction on peer0.orga peer0.orgb..."
+# Invoke chaincode on peer0.org1 and peer0.org2
+echo "Sending invoke transaction on peer0.org1 peer0.org2..."
 chaincodeInvoke 0 1 0 2
 
-## Install chaincode on peer1.orgb
-echo "Installing chaincode on peer1.orgb..."
+## Install chaincode on peer1.org2
+echo "Installing chaincode on peer1.org2..."
 installChaincode 1 2
 
-# Query on chaincode on peer1.orgb, check if the result is 90
-echo "Querying chaincode on peer1.orgb..."
+# Query on chaincode on peer1.org2, check if the result is 90
+echo "Querying chaincode on peer1.org2..."
 chaincodeQuery 1 2 90
 
 echo
-echo "========= All GOOD, DEMO system execution completed =========== "
+echo "========= All GOOD, BYFN execution completed =========== "
 echo
 
 echo
