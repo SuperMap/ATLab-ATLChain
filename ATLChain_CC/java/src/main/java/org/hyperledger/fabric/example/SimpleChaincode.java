dcc5aecf9214d428f256429aa51a2edb0e29d9e7
stone-ch <great_cy_ang@163.com>
2019-09-05 18:08:53 +0800

    modify chaincode

--- ATLChain_CC/java/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
+++ ATLChain_CC/java/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
@@ -1,50 +1,24 @@
 package org.hyperledger.fabric.example;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import javax.xml.ws.Response;
-import java.net.URL;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.google.protobuf.ByteString;
 import io.netty.handler.ssl.OpenSsl;
-import org.csource.common.NameValuePair;
-import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.FileInfo;
-import org.csource.fastdfs.StorageClient;
-import org.csource.fastdfs.StorageServer;
-import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
-import org.geotools.data.DataStore;
-import org.geotools.data.DataStoreFinder;
-import org.geotools.data.FeatureSource;
-import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.hyperledger.fabric.shim.ledger.KeyModification;
 import org.hyperledger.fabric.shim.ledger.KeyValue;
 import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.filter.Filter;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SimpleChaincode extends ChaincodeBase {
 
     private static Log _logger = LogFactory.getLog(SimpleChaincode.class);
 
-    private static final URL confURL = SimpleChaincode.class.getResource("fdfs_client.conf");
-    
     @Override
     public Response init(ChaincodeStub stub) {
         return newSuccessResponse();
@@ -63,12 +37,6 @@ public class SimpleChaincode extends ChaincodeBase {
             if (func.equals("Get")) {
                 return Get(stub, params);
             }
-            if (func.equals("PutBin")) {
-                return PutBin(stub, params);
-            }
-            if (func.equals("GetBin")) {
-                return GetBin(stub, params);
-            }
             if (func.equals("PutByteArray")) {
                 return PutByteArray(stub, paramsByte);
             }
@@ -81,45 +49,13 @@ public class SimpleChaincode extends ChaincodeBase {
             if (func.equals("GetRecordByKey")) {
                 return GetRecordByKey(stub, params);
             }
-            if (func.equals("tryGeoTools")) {
-                return tryGeoTools(stub, params);
-            }
+
             return newErrorResponse("Invalid invoke function name. Expecting one of: [\"Put\", \"Get\", \"getHistoryByKey\"]");
         } catch (Throwable e) {
             return newErrorResponse(e);
         }
     }
 
-    private Response tryGeoTools(ChaincodeStub stub, List<String> args){
-        StringBuilder strBuilder = new StringBuilder();
-        try {
-            File file = new File("example.shp");
-            Map<String, Object> map = new HashMap<>();
-            map.put("url", file.toURI().toURL());
-
-            DataStore dataStore = DataStoreFinder.getDataStore(map);
-            String typeName = dataStore.getTypeNames()[0];
-
-            FeatureSource<SimpleFeatureType, SimpleFeature> source =
-                    dataStore.getFeatureSource(typeName);
-            Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
-
-            FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
-            try (FeatureIterator<SimpleFeature> features = collection.features()) {
-                while (features.hasNext()) {
-                    SimpleFeature feature = features.next();
-                    strBuilder.append(feature.getID());
-                    strBuilder.append(": ");
-                    strBuilder.append(feature.getDefaultGeometryProperty().getValue());
-                    // System.out.print(strBuilder);
-                }
-        }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return newSuccessResponse("invoke finished successfully" + strBuilder);
-    }
 
     // TODO verify record signature
     private boolean verify(String jsonStr, String signatureStr, String pubkeyPemStr){
@@ -134,26 +70,21 @@ public class SimpleChaincode extends ChaincodeBase {
 
     // API:Put records
     private Response Put(ChaincodeStub stub, List<String> args, List<byte[]> argsByte){
-        int argsNeeded = 5;
-        if (args.size() != 5){
+        int argsNeeded = 4;
+        if (args.size() != 4){
             return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
         }
         String putKey = args.get(0);
         String jsonStr = args.get(1);
         String signatureStr = args.get(2);
         String pubkeyPemStr = args.get(3);
-        byte[] fileContent = argsByte.get(4);
 
         if (!verify(jsonStr, signatureStr, pubkeyPemStr)){
             return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
         }
 
-        String filePath = fastDFSUploadFile(fileContent, "atl");
-
         stub.putStringState(putKey, jsonStr);
-        _logger.info("Transfer complete");
-        _logger.info("+++++++++++++++++++FilePath: " + filePath);
-        return newSuccessResponse("invoke finished successfully. FilePath: " + filePath);
+        return newSuccessResponse("invoke finished successfully. ");
     }
     
     // Put byte array data to ledger
@@ -187,29 +118,6 @@ public class SimpleChaincode extends ChaincodeBase {
         return newSuccessResponse(byteArray);
     }
 
-    
-    // Put binary string data on chain
-    private Response PutBin(ChaincodeStub stub, List<String> args){
-        int argsNeeded = 4;
-        if (args.size() != 4){
-            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
-        }
-        String putKey = args.get(0);
-        String jsonStr = args.get(1);
-        String signatureStr = args.get(2);
-        String pubkeyPemStr = args.get(3);
-
-        String binary = "1100001 1100010 1100011";
-        jsonStr = BinToString(binary);
-
-        if (!verify(jsonStr, signatureStr, pubkeyPemStr)){
-            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
-        }
-
-        stub.putStringState(putKey, jsonStr);
-        _logger.info("Transfer complete");
-        return newSuccessResponse("invoke finished successfully");
-    }
 
     // Query callback representing the query of a chaincode
     private Response Get(ChaincodeStub stub, List<String> args) {
@@ -239,20 +147,6 @@ public class SimpleChaincode extends ChaincodeBase {
         return newSuccessResponse(ByteString.copyFrom(strBuilder.toString(), UTF_8).toByteArray());
     }
 
-    // Query record by key, and change the binary string to string
-    private Response GetBin(ChaincodeStub stub, List<String> args) {
-        if (args.size() != 1) {
-            return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
-        }
-        String key = args.get(0);
-        String val = stub.getStringState(key);
-        String binVal = StrToBinstr(val);
-        if (val == null) {
-            return newErrorResponse(String.format("Error: state for %s is null", key));
-        }
-        _logger.info(String.format("Query Response:\nName: %s, Amount: %s\n", key, binVal));
-        return newSuccessResponse(binVal, ByteString.copyFrom(binVal, UTF_8).toByteArray());
-    }
 
     // API:Get the history of a key
     private Response GetHistoryByKey(ChaincodeStub stub, List<String> args) {
@@ -296,77 +190,37 @@ public class SimpleChaincode extends ChaincodeBase {
         return newSuccessResponse(val, ByteString.copyFrom(val, UTF_8).toByteArray());
     }
 
-    // Binary String to string
-    private static String BinToString(String binary) { 
-        String[] tempStr = binary.split(" "); 
-        char[] tempChar = new char[tempStr.length]; 
-        for(int i = 0; i < tempStr.length; i++) { 
-            tempChar[i] = BinstrToChar(tempStr[i]); 
-        } 
-        return String.valueOf(tempChar); 
-    }
-
-    // Binary String to int array
-    private static int[] BinstrToIntArray(String binStr) { 
-        char[] temp = binStr.toCharArray(); 
-        int[] result = new int[temp.length];  
-        for(int i = 0; i < temp.length; i++) {  
-            result[i] = temp[i] - 48;       
-        } 
-        return result; 
-    }
-    
-    // Binary to char
-    private static char BinstrToChar(String binStr){ 
-        int[] temp = BinstrToIntArray(binStr); 
-        int sum = 0; 
-        for(int i = 0; i < temp.length; i++){ 
-            sum += temp[temp.length - 1 - i] << i; 
-        } 
-        return (char)sum; 
-    }
-
-    // String to Binary String, splited by space
-	private static String StrToBinstr(String str) {
-		char[] strChar = str.toCharArray();
-		String result = "";
-		for (int i = 0; i < strChar.length; i++) {
-			result += Integer.toBinaryString(strChar[i]) + " ";
-		}
-		return result;
-	}
-
     // ============================== FastDFS Start ================================
 
-    private String fastDFSUploadFile(byte[] fileContent, String fileExtName) {
-		String fileIds[] = new String[2];
-        StorageServer storageServer = null;
-        TrackerServer trackerServer = null;
-		try {
-            _logger.info("confURL.toString(): ");
-			// ClientGlobal.init(confURL.toURI().toString());
-			ClientGlobal.init("/fdfs_client.conf");
-			TrackerClient tracker = new TrackerClient();
-			trackerServer = tracker.getConnection();
-			StorageClient storageClient = new StorageClient(trackerServer, storageServer);
-			fileIds = storageClient.upload_file(fileContent, fileExtName, null);
-			if (fileIds == null) {
-				return "DFSUploadFile failed";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (storageServer != null)
-					storageServer.close();
-				if (trackerServer != null)
-					trackerServer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return "uploadfile path:" + fileIds[0] + "/" + fileIds[1];
-	}
+    // private String fastDFSUploadFile(byte[] fileContent, String fileExtName) {
+	// 	String fileIds[] = new String[2];
+    //     StorageServer storageServer = null;
+    //     TrackerServer trackerServer = null;
+	// 	try {
+    //         _logger.info("confURL.toString(): ");
+	// 		// ClientGlobal.init(confURL.toURI().toString());
+	// 		ClientGlobal.init("/fdfs_client.conf");
+	// 		TrackerClient tracker = new TrackerClient();
+	// 		trackerServer = tracker.getConnection();
+	// 		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+	// 		fileIds = storageClient.upload_file(fileContent, fileExtName, null);
+	// 		if (fileIds == null) {
+	// 			return "DFSUploadFile failed";
+	// 		}
+	// 	} catch (Exception e) {
+	// 		e.printStackTrace();
+	// 	} finally {
+	// 		try {
+	// 			if (storageServer != null)
+	// 				storageServer.close();
+	// 			if (trackerServer != null)
+	// 				trackerServer.close();
+	// 		} catch (IOException e) {
+	// 			e.printStackTrace();
+	// 		}
+	// 	}
+	// 	return "uploadfile path:" + fileIds[0] + "/" + fileIds[1];
+	// }
 
 	// private Response fastDFSDownloadFile(ChaincodeStub stub, List<String> args) { // 下载文件
 	// 	if (args.size() != 3) {
